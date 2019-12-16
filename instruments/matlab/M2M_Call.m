7f97af00da24029ac3a25a2ae7b40cfeed6f9af2
Craig Risien <crisien@coas.oregonstate.edu>
2019-12-16 14:07:18 -0800

    major refactoring of the matlab m2m toolbox

--- instruments/matlab/M2M_Call.m
+++ instruments/matlab/M2M_Call.m
@@ -1,53 +1,136 @@
-function [nc_urls] = M2M_Call(uframe_dataset_name,data_url,start_date,end_date,options)
+function [nclist] = M2M_Call(uframe_dataset_name,start_date,end_date,options)
+%.. 2019-10-XX: CMRisien. original code.
+%.. 2019-11-04: RADesiderio.
+%..             (a) put in a for loop to iterate over multiple streams
+%..             (b) restored the first try-catch block that CMR removed.
+%..             (c) restored the incompleteness check that CMR removed.
+%..             (d) removed data_url from the input argument list;
+%..             (e) variable m2m_url transferred in from function M2M_URLs;
+%..             (f) transferred thredds_url out to be a variable in M2M_Data;
+%..             (g) changed output argument from nc_urls to nclist so that the
+%..                 option of using different sources in the websave calls
+%..                 in M2M_Data could be implemented.
+%.. 2019-12-16: RADesiderio.
+%..             (a) reverted from a previous version which had added a urlread
+%..                 branch to work with matlab version R2019b. this branch no
+%..                 longer exists in the code; compatibility with R2019b is
+%..                 achieved by changing the 'options' input argument in the
+%..                 calling program. See Notes.
+%..             (b) added parsing for catalog and async URLs from m2m_response
+%
+%.. NOTES:
+%..
+%.. Use the following sentence in the calling program to ensure compatibility
+%.. with all (recent) versions of Matlab (tested up to R2019b). A different 
+%.. value can be used for the Timeout setting. Note that if the webread call
+%.. fails using the following weboptions call and the user is running on 
+%.. linux, an additional CertificateFilename setting to '' (empty character
+%.. string) may be required.
+%
+% options = weboptions('HeaderFields', {'Authorization', ...
+%    ['Basic ' matlab.net.base64encode([api_key ':' api_token])]}, 'Timeout', 120);
+%
 
-thredds_url = "https://opendap.oceanobservatories.org/thredds/dodsC";
+m2m_url = "https://ooinet.oceanobservatories.org/api/m2m/12576/sensor/inv/";
+data_url = strcat(m2m_url, uframe_dataset_name);
 
-%.. Make M2M Call
-m2m_response = webread(data_url, "beginDT", start_date, "endDT", end_date, options);
-
-for ii = 1:1800
+%.. Make M2M Call(s)
+nclist(1:length(data_url)) = {""};
+response_status(1:length(data_url)) = "No Uframe data found.";
+for jj = 1:length(data_url)
+    %.. request the data and start monitoring for a completed request.
+    %.. .. cannot distinguish between the following two errors, because they
+    %.. .. both give identical error messages, including "NOT FOUND":
+    %.. .. (1) no data in requested time range
+    %.. .. (2) incorrect spelling of dataset name.
     try
-        check_complete = webread(join([m2m_response.allURLs{2}, "status.txt"], "/"));
-        if strip(check_complete) == "complete"
-            fprintf("request completed\n");
-            response_status = 'request complete';
-            break
-        else
+        m2m_response = webread(data_url(jj), "beginDT", start_date, "endDT", end_date, options);
+        response_status(jj) = 'M2M REQUEST MADE, not yet completed:';
+        disp(response_status{jj});
+        %.. the following two URLs are not always presented in the same order, so:
+        catalogURL = string(m2m_response.allURLs(contains(m2m_response.allURLs, 'catalog')));
+        asyncURL = string(m2m_response.allURLs(contains(m2m_response.allURLs, 'async')));
+    catch ME
+        %.. comment out ME diagnostics, display abbreviated diagnostic instead
+        %disp(ME.identifier);
+        %disp(ME.message);
+        
+        %.. for NUTNR requests, several different datastreams are required to
+        %.. span the entire time that these instruments were deployed. These
+        %.. datastreams do not exist for all of these time periods, therefore
+        %.. throw a warning and not a fatal error if no Uframe data are found.
+        %.. (For example, the SUNA replaced the ISUS on surface moorings, and
+        %.. while the data collected are identical, the data formats are not,
+        %.. requiring different streams that were applicable at different
+        %.. times).
+        response_status(jj) = ['WARNING: ' ...
+            'No UFrame data found for datastream ' uframe_dataset_name{jj}];
+        disp(['uframe_m2m_status: ' response_status{jj}]);
+        disp(' ');
+        continue
+    end
+    
+    for ii = 1:1800
+        try
+            check_complete = webread(join([asyncURL, "status.txt"], "/"));
+            if strip(check_complete) == "complete"
+                response_status(jj) = 'request complete';
+                fprintf("done\n");
+                break
+            else
+                pause(1);
+                response_status(jj) = 'request incomplete';
+                if mod(ii,10)==0, fprintf('%u ', ii); end
+            end
+        catch
             pause(1);
-            response_status = 'request incomplete';
+            response_status(jj) = 'request incomplete';
             if mod(ii,10)==0, fprintf('%u ', ii); end
         end
-    catch
-        pause(1);
-        response_status = 'request incomplete';
-        if mod(ii,10)==0, fprintf('%u ', ii); end
     end
+    disp(['uframe_m2m_status: ' response_status{jj}]);
+    if ~strcmp(response_status, 'request complete')
+        disp(' ');
+        error(['***WARNING: ' uframe_dataset_name{jj} ' NEEDS TO BE RE-RUN***']);
+    end
+    
+    % now put together the list of available NetCDF files from the THREDDS server
+    catalog = webread(catalogURL);
+    nc_all = regexp(catalog, '<a href=''([^>]+.nc)''>', 'tokens'); % cell elements are themselves cells
+    
+    %.. 2019 fdchp case: m2m request is successfully completed, but
+    %..                  there are no netcdf files in the catalog.
+    if isempty(nc_all)
+        response_status(jj) = 'WARNING: m2m request completed: no uframe netcdf files';
+        disp(['uframe_m2m_status: ' response_status{jj}]);
+        continue
+    else
+        response_status(jj) = 'successful';
+        disp(['uframe_m2m_status: ' response_status{jj}]);
+    end
+    
+    disp(['THREDDS Catalog URL: ' catalogURL])
+    disp(' ');
+    
+    nc_all = [nc_all{:}]';  % nc_all is now a cell array of character vectors
+    nc_all = cellfun(@(x) x(22:end), nc_all, 'UniformOutput', 0); % drop 'catalog.html?dataset=' from URL
+    
+    %.. prune the list of mapped files to our instrument of interest by eliminating
+    %.. files associated with other instruments used to calculate data products
+    strings_to_match = sprintf('.*/deployment.*%s.*\\.nc$', strrep(uframe_dataset_name(jj), '/', '-'));
+    nc_all = cellfun(@(x) regexp(x, strings_to_match, 'match'), nc_all, 'UniformOutput', 0);
+    nc_all(cellfun('isempty', nc_all)) = [];  % cell elements are themselves cells
+    nclist{jj} = string(nc_all(:));  % string array
 end
-
-% now put together the list of available NetCDF files from the THREDDS server
-catalog = webread(m2m_response.allURLs{1});
-nclist = regexp(catalog, '<a href=''([^>]+.nc)''>', 'tokens'); % cell elements are themselves cells
-
+%.. stop execution if no uframe data found
+if ~any(contains(lower(response_status), 'successful'))
+    error('***NO UFRAME DATA FOUND IN ANY DATASTREAM(S)***');
+end
+%.. consolidate nclist into one string array
+nclist = vertcat(nclist{:});
+nclist(cellfun('isempty', nclist)) = [];
 if isempty(nclist)
-    response_status = 'm2m request completed: no uframe netcdf files';
-    disp(['uframe_m2m_status: ' response_status]);
-    return
-else
-    response_status = 'successful';
-    disp(['uframe_m2m_status: ' response_status]);
+    error('***NO NETCDF FILES FOUND IN ANY DATASTREAM(S)***');
 end
 
-disp(['THREDDS Catalog URL: ' m2m_response.allURLs{1}])
-
-nclist = [nclist{:}]';  % nclist is now a cell array of character vectors
-nclist = cellfun(@(x) x(22:end), nclist, 'UniformOutput', 0); % drop 'catalog.html?dataset=' from URL
-%.. create a URL mapping of the files
-nc_urls_all = strcat(thredds_url, "/", nclist);  % string array
-
-%.. prune the list of mapped files to our instrument of interest by eliminating
-%.. files associated with other instruments used to calculate data products
-strings_to_match = sprintf('.*/deployment.*%s.*\\.nc$', strrep(uframe_dataset_name, '/', '-'));
-nc_urls = cellfun(@(x) regexp(x, strings_to_match, 'match'), nc_urls_all, 'UniformOutput', 0);
-nc_urls(cellfun(@isempty, nc_urls)) = [];  % cell elements are themselves cells
-nc_urls = string(nc_urls(:));  % string array
-end
+end  % function
---
+++ instruments/matlab/M2M_Data.m
@@ -0,0 +1,153 @@
+function [variables, mtime, netcdfFilenames] = M2M_Data(variables, nclist, opendap)
+%.. 2019-10-XX: CMRisien. original code.
+%.. 2019-10-23: RADesiderio.
+%..             the original version of this code was in a main program written
+%..             by Craig Risien. I isolated it into this subroutine and used
+%..             horzcat instead of specifying indices. 
+%..       NOTE: (a) if the dataset is 1D, then the netcdf files contain
+%..                 column vectors (time increases by increasing row index).
+%..             (b) if the dataset is 2D, then the netcdf files contain 2D 
+%..                 arrays in which time increases by increasing column index.
+%.. 2019-11-04: RADesiderio. 
+%..             (a) replaced nc_urls with nclist in the input argument list;
+%..             (b) variable thredds_url transferred in from function M2M_Call;
+%..             (c) added capability to read in the data from downloaded netcdf files
+%..             (d) added calling argument opendap
+%.. 2019-11-27: RADesiderio.
+%..             (a) sorted nclist chronologically by deployment
+%..             (b) appended deployment to variable list
+%..             (c) read lat and lon from global attributes (non-mobile assets)
+%
+%.. INPUT: opendap is a switch. if it is 1 or true, the opendap call is made.
+%..                             if it is 0 or false, nc files are downloaded.
+%..                             DEFAULT IS TRUE.         
+%..        these cases have the same outputs.
+%
+%
+%.. netcdf files containing data from surface moorings formerly had variables
+%.. named 'lat' and 'lon'; now, they do not (except for some metbk and fdchp
+%.. data streams). these netcdf files do have lat and lon mins and maxes 
+%.. listed as global attributes (as do those metbk and fdchp exceptions).
+%.. Therefore a section has been included to read in those global attribute
+%.. values.
+%
+%.. this function was written to be run last in the sequence:
+%.. .. M2M_URLs
+%.. .. M2M_Call
+%.. .. M2M_Data
+
+if nargin==2
+    opendap = true;
+end
+
+%.. additional variables to add, common to all instruments; enumerate here.
+%.. .. lat and lon have been removed as netcdf variables for instruments
+%.. .. not on mobile assets (gliders); will need to parse the netcdf
+%.. .. files' global attributes separately to get lat and lon.
+additionalVariables(1).name  = 'deployment';
+additionalVariables(1).data  = [];
+additionalVariables(1).units = 'unitless';
+%additionalVariables(2).name  = 'fill in this space as appropriate';
+%additionalVariables(2).data  = [];
+%additionalVariables(2).units = 'fill in this space as appropriate';
+
+if any(contains({variables.name}, 'lat'))
+    isMobileAsset = true;
+    %.. lat and lon are ordinary netcdf variables.
+    %.. however, the mins and maxes are available from global attributes.
+    %.. latMean and lonMean must be the LAST 2 VARIABLES.
+    last2Variables(1).name  = 'latMean';
+    last2Variables(1).data  = [];
+    last2Variables(1).units = 'degrees_north';
+    last2Variables(2).name  = 'lonMean';
+    last2Variables(2).data  = [];
+    last2Variables(2).units = 'degrees_east';
+else
+    isMobileAsset = false;
+    %.. lat and lon must be the LAST 2 VARIABLES.
+    last2Variables(1).name  = 'lat';
+    last2Variables(1).data  = [];
+    last2Variables(1).units = 'degrees_north';
+    last2Variables(2).name  = 'lon';
+    last2Variables(2).data  = [];
+    last2Variables(2).units = 'degrees_east';
+end
+
+if isrow(variables)
+    variables = [variables additionalVariables last2Variables];
+elseif iscolumn(variables)
+    variables = [variables; additionalVariables; last2Variables];
+else
+    error('Input structure ''variables'' must be a vector');
+end
+
+%.. determine filenames
+%.. (a) for sorting nclist and ncfiles chronologically
+%.. (b) in case ncfiles are downloaded (opendap==false)
+netcdfFilenames = split(nclist, ["/", "\"], 2);
+netcdfFilenames = netcdfFilenames(:, end);
+%.. these begin with "deploymentXXXX_" (not 5X) where XXXX is deployment #
+[netcdfFilenames, idx] = sort(netcdfFilenames);
+%.. sort nclist so that data will be read in chronologically by deployment
+nclist = nclist(idx);
+
+%.. create a URL mapping of the files
+thredds_url = "https://opendap.oceanobservatories.org/thredds/dodsC";
+nc_urls = strcat(thredds_url, "/", nclist);  % string array
+
+for i = 1:length(nclist)
+    if opendap
+        source = char(nc_urls(i));
+    else
+        netcdfFilenames(i) = websave( ...
+            netcdfFilenames(i), strrep(nc_urls{i,1}, 'dodsC', 'fileServer'));
+        source = netcdfFilenames(i);
+    end
+    %.. first variable is always time; pull it out of the following
+    %.. loop so that nans can be assigned if a particular stream is
+    %.. missing a variable (some nutnr cases)
+    ncTime = ncread(source, variables(1).name);
+    variables(1).data = horzcat(variables(1).data, ncTime');
+    %.. if length(variables)==3 no error is thrown, and loop is not executed
+    %.. (variables lat and lon are treated differently than the others);
+    %.. subtract 2 because lat and lon are read from the global attributes
+    for j = 2:length(variables)-2  
+        try
+            data = ncread(source, variables(j).name);
+        catch
+            %.. the following statement will cause an error at the first horzcat
+            %.. statement if the missing variable is 2D
+            data = nan(size(ncTime));
+        end
+        [~, col]=size(data);
+        if col > 1
+            %.. if the next line is referenced in a runtime error, see
+            %.. comment immediately after the catch statement above.
+            variables(j).data = horzcat(variables(j).data, data);
+        else
+            variables(j).data = horzcat(variables(j).data, data');
+        end
+    end
+    %.. netdcf files for non-mobile assets have lat and lon information
+    %.. under global attributes; these will be a little bit different
+    %.. for each deployment.
+    latMin = ncreadatt(source, '/', 'geospatial_lat_min');
+    latMax = ncreadatt(source, '/', 'geospatial_lat_max');
+    if ~isMobileAsset && latMin~=latMax
+        disp('Warning! latMin and latMax don''t agree!');
+    end
+    lat = (latMin + latMax) / 2;
+    data = zeros(size(ncTime)) + lat;
+    variables(j+1).data = horzcat(variables(j+1).data, data');
+    %
+    lonMin = ncreadatt(source, '/', 'geospatial_lon_min');
+    lonMax = ncreadatt(source, '/', 'geospatial_lon_max');
+    if ~isMobileAsset && lonMin~=lonMax
+        disp('Warning! lonMin and lonMax don''t agree!');
+    end
+    lon = (lonMin + lonMax) / 2;
+    data = zeros(size(ncTime)) + lon;
+    variables(j+2).data = horzcat(variables(j+2).data, data');
+end
+%.. first variable is always time; convert to matlab datenumber [day]
+mtime=datenum(1900,1,1,0,0,0)+(variables(1).data/60/60/24);

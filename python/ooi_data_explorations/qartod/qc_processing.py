b501de21783ae5af4d5e8811a9b47cf79b39b267
Christopher Wingard <wingardc@oregonstate.edu>
2021-06-08 14:48:21 -0700

    Reorganizing the QARTOD folder and continuing build-out

---
+++ python/ooi_data_explorations/qartod/qc_processing.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author Christopher Wingard
+@brief Calculates QARTOD test ranges and creates the resulting tables used by
+    the OOI QC lookup functions implementing the QARTOD testing.
+"""
+import numpy as np
+import pandas as pd
+
+from ooi_data_explorations.qartod.climatology import Climatology
+from ooi_data_explorations.qartod.gross_range import GrossRange
+
+
+def format_climatology(param, clm, sensor_range, site, node, sensor, stream, source):
+    """
+    Creates a dictionary object that can later be saved to a CSV formatted
+    file for use in the Climatology lookup tables.
+
+    :param param: parameter name of the variable for the calculated user range
+    :param clm:
+    :param sensor_range:
+    :param site: Site designator, extracted from the first part of the reference
+        designator
+    :param node: Node designator, extracted from the second part of the reference
+        designator
+    :param sensor: Sensor designator, extracted from the third and fourth part of
+        the reference designator
+    :param stream: Stream name that contains the data of interest
+    :param source: Notes or comments about how the Gross Range values were
+        obtained
+    :return qc_dict: dictionary with the sensor and user gross range values
+        added in the formatting expected by the QC lookup
+    """
+    # create the lookup dictionary
+    var_explained = clm.regression['variance_explained']
+    qc_dict = {
+        'subsite': site,
+        'node': node,
+        'sensor': sensor,
+        'stream': stream,
+        'parameters': {'inp': param, 'tinp': 'time', 'zinp': 'None'},
+        'climatologyTable': 'climatology_tables/{}-{}-{}-{}.csv'.format(site, node, sensor, param),
+        'source': source + '. The variance explained by the climatological model is {:.1%}.'.format(var_explained[0])
+    }
+
+    # create the climatology table
+    header_str = ''
+    value_str = '[0, 0]'
+    for idx, mu in enumerate(clm.monthly_fit):
+        # use the index number to create the header row
+        header_str += ',[{}, {}]'.format(idx+1, idx+1)
+
+        # calculate the climatological ranges
+        cmin = mu - clm.monthly_std.values[idx] * 3
+        if cmin < sensor_range[0]:
+            cmin = sensor_range[0]
+
+        cmax = mu + clm.monthly_std.values[idx] * 3
+        if cmax > sensor_range[1]:
+            cmax = sensor_range[1]
+
+        # append the data to ranges
+        value_str += ',[{:.2f}, {:.2f}]'.format(cmin, cmax)
+
+    clm_table = header_str + '\n' + value_str
+
+    return qc_dict, clm_table
+
+
+def process_climatology(ds, params, sensor_range, **kwargs):
+    """
+
+    :param ds:
+    :param params:
+    :param sensor_range:
+    :param kwargs:
+    :return:
+    """
+    # process the optional keyword arguments
+    site = kwargs.get('site')
+    node = kwargs.get('node')
+    sensor = kwargs.get('sensor')
+    stream = kwargs.get('stream')
+    source = kwargs.get('source')
+
+    # initialize the Climatology class
+    clm = Climatology()
+
+    # create an empty pandas dataframe to hold the results
+    clm_lookup = pd.DataFrame()
+    clm_tables = []
+
+    # loop through the parameter(s) of interest
+    sensor_range = np.atleast_2d(sensor_range).tolist()
+    for idx, param in enumerate(params):
+        # calculate the 2-cycle climatology for the parameter of interest
+        clm.fit(ds, param)
+
+        # create the formatted dictionary for the lookup tables
+        qc_dict, clm_table = format_climatology(param, clm, sensor_range[idx], site, node, sensor, stream, source)
+
+        # append the dictionary to the dataframe
+        clm_lookup = clm_lookup.append(qc_dict, ignore_index=True)
+        clm_tables.append(clm_table)
+
+    # return the results
+    return clm_lookup, clm_tables
+
+
+def calc_gross_range(ds, param, sensor_range):
+    """
+    Simple function to calculate the user gross range values based on the
+    long-term average of the parameter(s) of interest plus/minus 3 standard
+    deviations (the statistical rule of three). The user range is used in the
+    QARTOD Gross Range test to set a suspect flag for data points that lie
+    outside of the defined range.
+
+    :param ds: xarray dataset with the parameter(s) of interest
+    :param param: list of the parameter names to calculate the ranges for
+    :param sensor_range: vendor specified sensor range, used to set the QARTOD
+        fail range portion of the Gross Range test
+    :return user_range: resulting user ranges, used to set the QARTOD suspect
+        range portion of the Gross Range test
+    """
+    gr = GrossRange(fail_min=sensor_range[0], fail_max=sensor_range[1])
+    gr.fit(ds, param, 3)
+    user_range = [gr.suspect_min, gr.suspect_max]
+    return user_range
+
+
+def format_gross_range(param, sensor_range, user_range, site, node, sensor, stream, source):
+    """
+    Creates a dictionary object that can later be saved to a CSV formatted
+    file for use in the Gross Range lookup tables.
+
+    :param param: parameter name of the variable for the calculated user range
+    :param sensor_range: default sensor, or fail range, usually referenced
+        from the vendor documentation
+    :param user_range: user range, or sensor range, calculated from the data
+    :param site: Site designator, extracted from the first part of the reference
+        designator
+    :param node: Node designator, extracted from the second part of the reference
+        designator
+    :param sensor: Sensor designator, extracted from the third and fourth part of
+        the reference designator
+    :param stream: Stream name that contains the data of interest
+    :param source: Notes or comments about how the Gross Range values were
+        obtained
+    :return qc_dict: dictionary with the sensor and user gross range values
+        added in the formatting expected by the QC lookup
+    """
+    # create the dictionary
+    qc_dict = {
+        'subsite': site,
+        'node': node,
+        'sensor': sensor,
+        'stream': stream,
+        'parameter': {
+            'inp': param
+        },
+        'qcConfig': {
+             'qartod': {
+                 'gross_range_test': {
+                     'suspect_span': ['{}'.format(user_range[0]), '{}'.format(user_range[1])],
+                     'fail_span': ['{}'.format(sensor_range[0]), '{}'.format(sensor_range[1])]
+                 }
+             }
+         },
+        'source': source
+    }
+    return qc_dict
+
+
+def process_gross_range(ds, params, sensor_range, **kwargs):
+    """
+
+    :param ds:
+    :param params:
+    :param sensor_range:
+    :param kwargs:
+    :return:
+    """
+    # process the optional keyword arguments
+    site = kwargs.get('site')
+    node = kwargs.get('node')
+    sensor = kwargs.get('sensor')
+    stream = kwargs.get('stream')
+    source = kwargs.get('source')
+
+    # create an empty pandas dataframe to hold the results
+    gross_range = pd.DataFrame()
+
+    # loop through the parameter(s) of interest
+    sensor_range = np.atleast_2d(sensor_range).tolist()
+    for idx, param in enumerate(params):
+        # calculate the user range
+        user_range = calc_gross_range(ds, param, sensor_range[idx])
+        # create the formatted dictionary
+        qc_dict = format_gross_range(param, sensor_range[idx], user_range, site, node, sensor, stream, source)
+        # append the dictionary to the dataframe
+        gross_range = gross_range.append(qc_dict, ignore_index=True)
+
+    # return the results
+    return gross_range

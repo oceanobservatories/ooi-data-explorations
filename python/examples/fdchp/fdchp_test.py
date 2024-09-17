0e21b68a4fb103061f01e5b7ddfa70e5ad484219
Joffrey Peters <joffrey.peters@whoi.edu>
2024-09-17 15:15:39 +0000

    Adding plots.

--- python/examples/fdchp/fdchp_test.py
+++ python/examples/fdchp/fdchp_test.py
@@ -5,6 +5,7 @@ from glob import glob
 import numpy as np
 from fdchp_utils import particles_to_pandas, read_file, read_file_to_pandas
 from process_fdchp import process_fdchp
+from plot_fdchp import plot_x_velocity_vs_wind_speed, plot_wave_height_vs_wind_speed
 
 PA=0
 EA=0
@@ -25,8 +26,16 @@ sigH=[]
 data = []
 # file_path = '/c/Data/flux_calculations/FDCHPTester'
 
-directory = '/home/jovyan/ooi/uncabled/GI01SUMO/R00008/instruments/dcl12/FDCHP_sn144904'
-files = glob(os.path.join(directory, '*/*.dat'))
+# directory = '/home/jovyan/ooi/uncabled/GI01SUMO/R00008/instruments/dcl12/FDCHP_sn144904/D202105'
+directory = '/home/jovyan/ooi/uncabled/GI01SUMO/R00008/instruments/dcl12/FDCHP_sn144904/D202110'
+
+files = glob(os.path.join(directory, '*.dat'))
+output_filepath = "fluxes"
+output_filename = "fluxes{}"
+
+if not os.path.exists(output_filepath):
+    os.makedirs(output_filepath)
+print("Processing {} files.".format(len(files)))
 incr=0
 print("Start time: {}".format(datetime.now()))
 # for incr in np.arange(24):
@@ -40,7 +49,7 @@ for filename in files:
     # else:
     #     filename = os.path.join(file_path, 'fdchp_20200818_'+ str(incr)+ '0200.dat')
 
-    print("Processing file: {}".format(filename))
+    print("Processing file {}: {}".format(incr + 1, filename))
     try:
         data = read_file(filename)
     except Exception as e:
@@ -57,7 +66,7 @@ for filename in files:
     # Compute flux data
     #*****************************************   
     try:
-        fluxes, Uearth, waveheight = process_fdchp(raw_data, lat, Rwaves, file_suffix=str(incr))
+        fluxes, Uearth, waveheight = process_fdchp(raw_data, lat, Rwaves, flux_filepath=os.path.join(output_filepath, output_filename.format(incr)))
     except Exception as e:
         # Error processing data; probably too few datapoints
         print("Error processing fdchp dataset: {}".format(e))
@@ -72,6 +81,11 @@ for filename in files:
 uw = np.array(uw)
 U = np.array(U)
 sigH = np.array(sigH)
+
+
+plot_x_velocity_vs_wind_speed(U, uw)
+
+plot_wave_height_vs_wind_speed(U, sigH)
 
 print("End time: {}".format(datetime.now()))
 print("Fin")
---
+++ python/examples/fdchp/plot_fdchp.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_x_velocity_vs_wind_speed(speeds, x_velocities):
+    fig, ax = plt.subplots()
+    ax.scatter(speeds, x_velocities, marker='o', alpha=0.5)
+    ax.set_xlabel('U (m/s)')
+    ax.set_xlim(0, 20)
+    ax.set_ylabel('-<uw> (m/s)')
+    ax.set_ylim(0, 1.0)
+    plt.show()
+    
+def plot_wave_height_vs_wind_speed(speeds, wave_heights):
+    fig, ax = plt.subplots()
+    ax.scatter(speeds, wave_heights, marker='o', alpha=0.5)
+    ax.set_xlabel('U (m/s)')
+    ax.set_xlim(0, 15)
+    ax.set_ylabel('$\sigma_H$ (m)')
+    ax.set_ylim(0, 4)
+    plt.show()
+

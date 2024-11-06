74c113fc4f5a07cf864e0effbf31d4a901f16c07
Joffrey Peters <joffrey.peters@whoie.edu>
2024-11-06 18:52:07 +0000

    Minor updates to plotting and some comments added for clarity of  parameters.

--- python/examples/fdchp/plot_fdchp.py
+++ python/examples/fdchp/plot_fdchp.py
@@ -7,9 +7,11 @@ def plot_x_velocity_vs_wind_speed(speeds, x_velocities):
     ax.scatter(speeds, x_velocities, marker='o', alpha=0.5)
     ax.set_xlabel('U (m/s)')
     ax.set_xlim(0, 20)
-    ax.set_ylabel('-<uw> (m/s)')
-    ax.set_ylim(0, 1.0)
+    ax.set_ylabel('<uw> (m/s)')
+    ax.set_ylim(-1.0, 0.0)
     plt.show()
+    
+    
     
 def plot_wave_height_vs_wind_speed(speeds, wave_heights):
     fig, ax = plt.subplots()
